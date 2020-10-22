c599b1f0880d3d329c7557e52b42637cb19376a6
brendan802 <67173637+brendan802@users.noreply.github.com>
2020-10-22 19:54:28 -0400

    updates 10/22

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
+//imports
 const express = require('express');
 const path =require ('path');
-
+//global variables
 const app =express()
 const port = process.env.PORT || 5000
 
@@ -10,6 +11,7 @@ app.get('*', (request, response) => {
     response.sendFile(path.resolve('.client/build/index.html'))
 })
 
+//server start/console log
 app.listen(port, () => {
     console.log(`Listening on port: ${port}`)
 })
